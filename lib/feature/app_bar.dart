f2d0837e8052384e224f2bdde30c6d572544b448
Recep Oğuzhan Şenoğlu <recep.senoglu.34@gmail.com>
2024-01-05 17:57:16 +0300

    appBar sizes fixed

--- lib/feature/app_bar.dart
+++ lib/feature/app_bar.dart
@@ -1,19 +1,18 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../model/theme_model.dart';
+import '../utils/app_sizes.dart';
 
 class CalculatorAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CalculatorAppBar({super.key});
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => Size.fromHeight(AppSizes.getHeight(0.08));
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      toolbarHeight: 52,
       titleSpacing: 0,
       title: const Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -37,12 +36,34 @@ class _BrandName extends StatelessWidget {
 
     return Expanded(
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(15, 10, 0, 0),
+        padding: EdgeInsets.only(
+          left: AppSizes.getWidth(0.03),
+          top: AppSizes.getHeight(0.015),
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(strBrand, style: Theme.of(context).textTheme.titleMedium?.copyWith(fontSize: 16)),
-            Text(strCalculator, style: Theme.of(context).textTheme.titleSmall),
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: AppSizes.getWidth(0.35),
+              ),
+              child: FittedBox(
+                child: Text(
+                  strBrand,
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(fontSize: AppSizes.getHeight(0.02)),
+                ),
+              ),
+            ),
+            Text(
+              strCalculator,
+              style: Theme.of(context)
+                  .textTheme
+                  .titleSmall
+                  ?.copyWith(fontSize: AppSizes.getHeight(0.015)),
+            ),
           ],
         ),
       ),
@@ -57,16 +78,18 @@ class _ThemeSwitch extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<ThemeModel>(
       builder: (context, ThemeModel themeProvider, child) {
-        return CupertinoSwitch(
+        return Switch.adaptive(
           value: themeProvider.isDark,
           onChanged: (isDark) {
             themeProvider.changeTheme(isDark);
           },
           activeColor: const Color(0xFF363636),
-          trackColor: Colors.white,
-          thumbColor: themeProvider.isDark
-              ? const Color(0xFF75C0D0)
-              : const Color(0xFFEBB04F),
+          trackColor: MaterialStateProperty.all<Color>(Colors.black),
+          thumbColor: MaterialStateProperty.all<Color>(
+            themeProvider.isDark
+                ? const Color(0xFF75C0D0)
+                : const Color(0xFFEBB04F),
+          ),
         );
       },
     );
@@ -79,39 +102,56 @@ class _SolarPanels extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 18, 10, 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            ...List.generate(4, (index) {
-              return Padding(
-                padding: const EdgeInsets.only(left: 4),
-                child: Stack(
-                  children: [
-                    Container(
-                      width: 21,
-                      height: 21,
-                      margin: const EdgeInsets.only(left: 2.5, top: 2.5),
-                      decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.outline),
+      child: Container(
+        width: AppSizes.getWidth(0.2),
+        height: AppSizes.getHeight(0.08),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(
+          top: AppSizes.getHeight(0.03),
+          right: AppSizes.getWidth(0.03),
+        ),
+        child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          itemCount: 4,
+          itemBuilder: (context, index) {
+            final double size = AppSizes.getWidth(0.05);
+            final double margin = AppSizes.getWidth(0.007);
+
+            return Padding(
+              padding: EdgeInsets.only(
+                  left: index == 0 ? 0 : AppSizes.getWidth(0.01)),
+              child: Stack(
+                children: [
+                  Container(
+                    width: size,
+                    height: size,
+                    margin: EdgeInsets.only(left: margin, top: margin),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.outline,
+                      borderRadius: BorderRadius.circular(2),
                     ),
-                    Container(
-                      width: 21,
-                      height: 21,
-                      margin: const EdgeInsets.only(right: 2.5, bottom: 2.5),
-                      decoration: BoxDecoration(
-                          color: Theme.of(context).scaffoldBackgroundColor,
-                          border: Border.all(
-                              color: Theme.of(context).colorScheme.outline,
-                              width: 2),
-                          borderRadius: BorderRadius.circular(1)),
+                  ),
+                  Container(
+                    width: size,
+                    height: size,
+                    margin: EdgeInsets.only(right: margin, bottom: margin),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                      border: Border.all(
+                        color: Theme.of(context).colorScheme.outline,
+                        width: AppSizes.getWidth(0.005),
+                      ),
+                      borderRadius: BorderRadius.circular(2),
                     ),
-                  ],
-                ),
-              );
-            }),
-          ],
+                  ),
+                ],
+              ),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return SizedBox(width: AppSizes.getWidth(0.01));
+          },
         ),
       ),
     );
