cfdd13c3f09905ed08d2fa30f16de0235127d8b7
Recep Oğuzhan Şenoğlu <recep.senoglu.34@gmail.com>
2024-01-23 02:08:12 +0300

    ios files configurated

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/recepsenoglu/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/recepsenoglu/Documents/projects/flutterProjects/flutter_calculator"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/recepsenoglu/Documents/projects/flutterProjects/flutter_calculator/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.2"
+export "FLUTTER_BUILD_NUMBER=3"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC82ZTJlYTU4YTVjY2JlZmNmYWRjMmQ0YjdiODA0NWZjNmMyMGFiODEyLw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/recepsenoglu/Documents/projects/flutterProjects/flutter_calculator/.dart_tool/package_config.json"
